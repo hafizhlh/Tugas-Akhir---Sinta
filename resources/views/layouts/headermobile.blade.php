19d86abf9d43d09255840d53770d876115cf12b5
lisashii7 <91017706+lisashii7@users.noreply.github.com>
2023-04-13 11:15:05 +0700

    responsive

--- resources/views/layouts/headermobile.blade.php
+++ resources/views/layouts/headermobile.blade.php
@@ -1,7 +1,7 @@
 <div id="kt_header_mobile" class="header-mobile align-items-center header-mobile-fixed">
     <!--begin::Logo-->
     <a href="index.html">
-        <img alt="Logo" src="{{ asset('assets/media/logos/logo.png') }}" style="width: 179px"/>
+        <img alt="Logo" src="{{ asset('assets/media/logos/sintalogo.png') }}" style="width:65px"/>
     </a>
     <!--end::Logo-->
     <!--begin::Toolbar-->
@@ -12,9 +12,7 @@
         </button>
         <!--end::Aside Mobile Toggle-->
         <!--begin::Header Menu Mobile Toggle-->
-        <button class="btn p-0 burger-icon ml-4" id="kt_header_mobile_toggle">
-            <span></span>
-        </button>
+       
         <!--end::Header Menu Mobile Toggle-->
         <!--begin::Topbar Mobile Toggle-->
         <button class="btn btn-hover-text-primary p-0 ml-2" id="kt_header_mobile_topbar_toggle">
